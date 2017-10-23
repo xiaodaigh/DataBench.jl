b879ff6c6ce2e1218f525eebc52af9f0f8197310
susabi <zhuojia.dai@gmail.com>
2017-10-23 21:17:49 +1100

    added one SAC test

--- src/julia/indexedtable.jl_bench.jl
+++ src/julia/indexedtable.jl_bench.jl
@@ -1,4 +1,4 @@
-using IndexedTables, PooledArrays, NamedTuples, FastGroupBy
+using IndexedTables, PooledArrays, NamedTuples, FastGroupBy, SAC
 import Base.ht_keyindex
 # Pkg.checkout("JuliaDB")
 # Pkg.rm("TextParse")
@@ -48,9 +48,38 @@ function run_juliadb_bench(N::Int = Int64(2e9/8), K::Int = 100)
     :sum_mean2 => (@elapsed [sumby(DT, :id3, :v1), meanby(DT,:id3, :v3)]),
     :mean7_9_by_id4_1 => (@elapsed [meanby(DT, :id4, :v1), meanby(DT, :id4, :v2), meanby(DT, :id4, :v3)]),
     :mean7_9_by_id4_2 => (@elapsed [meanby(DT, :id4, :v1), meanby(DT, :id4, :v2), meanby(DT, :id4, :v3)]),
-    :sum7_9_by_id6_1 => (@elapsed [sumby(DT, :id6, :v1), sumby(DT, :id6, :v2), sumby(DT, :id6, :v3)]),
-    :sum7_9_by_id6_2 => (@elapsed [sumby(DT, :id6, :v1), sumby(DT, :id6, :v2), sumby(DT, :id6, :v3)])
+    :sum7_9_by_id6_1 => (@elapsed groupreduce(
+        x->x[1],
+        x->(x[2],x[3],x[4]),
+        (x,y)->(x[1]+y[1],x[2]+y[2],x[3]+y[3]),
+        zip(column(DT,:id6),column(DT,:v1),column(DT,:v2),column(DT,:v3)))),
+    :sum7_9_by_id6_2 => (@elapsed groupreduce(
+        x->x[1],
+        x->(x[2],x[3],x[4]),
+        (x,y)->(x[1]+y[1],x[2]+y[2],x[3]+y[3]),
+        zip(column(DT,:id6),column(DT,:v1),column(DT,:v2),column(DT,:v3))))
   )
 
   return julia_timings
 end
+
+# function run_sac_bench(N::Int = Int64(2e9/8), K::Int = 100)
+#   # create true dataset
+#   #julia_timings = Dict{Symbol, Float64}()
+#   timegen = @elapsed DT = createIndexedTable(N, K)
+#   sac_timings = Dict(
+#     :gen_syn => timegen,
+#     :sum1_1 => (@elapsed groupreduce(x->x[1], x->x[2], (x,y) -> x+y, zip(column(DT,:id1),column(DT,:v1)))),
+#     :sum1_2 => (@elapsed groupreduce(x->x[1], x->x[2], (x,y) -> x+y, zip(column(DT,:id1),column(DT,:v1)))),
+#     :sum2_1 => (@elapsed groupreduce(x->(x[1], x[2]), x -> x[3], (x, y) -> x+y, zip(column(DT,:id1),column(DT,:id2),column(DT,:v1)))),
+#     :sum2_2 => (@elapsed groupreduce(x->(x[1], x[2]), x -> x[3], (x, y) -> x+y, zip(column(DT,:id1),column(DT,:id2),column(DT,:v1)))),
+#     :sum_mean1 => (@elapsed groupreduce(x->x[1], x -> (x[2],x[3],1), (x, y) -> (x[1]+y[1],x[2]+y[2],x[3]+y3]), zip(column(DT,:id3),column(DT,:v1),column(DT,:v3)))),
+#     :sum_mean2 => (@elapsed groupreduce(x->x[1], x -> (x[2],x[3],1), (x, y) -> (x[1]+y[1],x[2]+y[2],x[3]+y3]), zip(column(DT,:id3),column(DT,:v1),column(DT,:v3)))),
+#     :mean7_9_by_id4_1 => (@elapsed [meanby(DT, :id4, :v1), meanby(DT, :id4, :v2), meanby(DT, :id4, :v3)]),
+#     :mean7_9_by_id4_2 => (@elapsed [meanby(DT, :id4, :v1), meanby(DT, :id4, :v2), meanby(DT, :id4, :v3)]),
+#     :sum7_9_by_id6_1 => (@elapsed [sumby(DT, :id6, :v1), sumby(DT, :id6, :v2), sumby(DT, :id6, :v3)]),
+#     :sum7_9_by_id6_2 => (@elapsed [sumby(DT, :id6, :v1), sumby(DT, :id6, :v2), sumby(DT, :id6, :v3)])
+#   )
+#
+#   return sac_timings
+# end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,7 @@
 using FastGroupBy, DataBench
 using Base.Test
 import DataFrames.DataFrame
+using SAC
 
 #using DataFrames, CSV
 #import Base.ht_keyindex
@@ -9,6 +10,7 @@ import DataFrames.DataFrame
 @time b = run_juliadb_bench(1_000_000, 100)
 @time a = R_bench(1_000_000, 100; libpath = "C:/Users/dzj/Documents/R/win-library/3.4")
 c = Dict(n => b[n]/a[n][1] for n in names(a))
+
 @test length(c) == 11
 
 @time b = run_juliadb_bench()
@@ -28,3 +30,14 @@ CSV.write(file1, abc);
 
 # files = open("test/results/"*readdir("test/results/")[2], "r")
 # abc1 = deserialize( files)
+#
+# using DataBench
+# dt = createIndexedTable(Int64(2e9/8), 100)
+# @time sumby(dt, :id6, :v1)
+# gc()
+# @time sumby(dt, :id6, :v1)
+# gc()
+# @time groupreduce(x->x[1], x->(x[2],1), (x,y) ->(x[1]+y[1],x[2]+y[2]), zip(column(dt,:id6), column(dt,:v1)))
+# gc()
+# @time groupreduce(x->x[1], x->(x[2],1), (x,y) ->(x[1]+y[1],x[2]+y[2]), zip(column(dt,:id6), column(dt,:v1)))
+# gc()
