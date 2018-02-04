b9f580c0c7477cd1cc750c962d8473311e49102e
susabi <zhuojia.dai@gmail.com>
2018-02-04 21:10:43 +1100

    improved dataframe generation code

--- src/julia/DataFrames.jl
+++ src/julia/DataFrames.jl
@@ -1,19 +1,20 @@
-using DataFrames, Distributions
+using DataFrames
 
 function createSynDataFrame(N::Int,K::Int)
     pool = "id".*dec.(1:K,3)
     pool1 = "id".*dec.(1:N÷K,10)
+    nums = round.(rand(100).*100, 4)
 
     df = DataFrame(
-        id1 = sample(pool,N),
-        id2 = sample(pool,N),
-        id3 = sample(pool1,N),
-        id4 = sample(1:K,N),
-        id5 = sample(1:K,N),
-        id6 = sample(1:(N/K),N),
-        v1 = sample(1:5,N),
-        v2 = sample(1:5,N),
-        v3 = sample(rand(round.(rand(Uniform(0,100),100),4), N)))
+        id1 = rand(pool,N),
+        id2 = rand(pool,N),
+        id3 = rand(pool1,N),
+        id4 = rand(1:K,N),
+        id5 = rand(1:K,N),
+        id6 = rand(1:(N/K),N),
+        v1 = rand(1:5,N),
+        v2 = rand(1:5,N),
+        v3 = rand(nums,N))
     return df
 end
 
@@ -21,16 +22,17 @@ end
 function createSynDataFrame_categoricalarray(N::Int,K::Int)
     pool = "id".*dec.(1:K,3)
     pool1 = "id".*dec.(1:N÷K,10)
+    nums = round.(rand(100)*100, 4)
 
     df = DataFrame(
-        id1 = sample(pool,N),
-        id2 = sample(pool,N),
-        id3 = sample(pool1,N),
-        id4 = sample(1:K,N),
-        id5 = sample(1:K,N),
-        id6 = sample(1:(N/K),N),
-        v1 = sample(1:5,N),
-        v2 = sample(1:5,N),
-        v3 = sample(rand(round.(rand(Uniform(0,100),100),4), N)))
+        id1 = rand(pool,N),
+        id2 = rand(pool,N),
+        id3 = rand(pool1,N),
+        id4 = rand(1:K,N),
+        id5 = rand(1:K,N),
+        id6 = rand(1:(N/K),N),
+        v1 = rand(1:5,N),
+        v2 = rand(1:5,N),
+        v3 = rand(nums, N))
     return df
 end
